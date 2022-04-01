7f5d9c53908792f16cf9b4a6d553dfe5c41d2384
lucianacappa <lucianacappa@gmail.com>
2022-04-01 19:18:06 +0200

    First commit

---
+++ pruebamss.py
@@ -0,0 +1,41 @@
+import numpy as np
+import cv2
+import mss
+import mss.tools
+
+#screenshot#
+for filename in sct.save():
+    print(filename)
+
+#Imagen de base, tamaño completo - el 0 representa escala de grises, 1 contempla transparencias#
+img = cv2.imread("filename", 0)
+
+#Imagen recortada, template#
+obj = cv2.imread("template.jpg", 0)
+h,w= obj.shape
+
+#lista de métodos a utilizar#
+methods = [cv2.TM_CCOEFF, cv2.TM_CCOEFF_NORMED, cv2.TM_CCORR,
+            cv2.TM_CCORR_NORMED, cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]
+
+for method in methods:
+    img2=img.copy()
+
+    result=cv2.matchTemplate(img2,obj,method)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+
+    #  Los metodos TM_SQDIFF y TM_SQDIFF_NORMED buscan valores mínimos#
+
+    if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+        location = min_loc
+    else:
+        location = max_loc
+
+        bottom_right = (location[0] + w, location[1] + h)
+        cv2.rectangle(img2, location, bottom_right, 255, 5)
+        cv2.imshow('Match', img2)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+
+
